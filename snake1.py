3d282e7737c6816421186ddcfac5098de641ab4e
Nathan Jean <nathanjean@genetixmedia.com>
2016-12-21 11:52:46 -0500

    updated comments

--- snake1.py
+++ snake1.py
@@ -8,14 +8,14 @@ from curses import KEY_RIGHT, KEY_LEFT, KEY_DOWN, KEY_UP
 from random import randint
 
 ''' #2 Define some variables '''
-WIDTH = 35
+WIDTH = 35 # total game cell width and height
 HEIGHT = 20
-MAX_X = WIDTH - 2
+MAX_X = WIDTH - 2 # sets max inside the game, so the snake wont hit the border
 MAX_Y = HEIGHT - 2
-SNAKE_LENGTH = 5
+SNAKE_LENGTH = 5 # Starting snake length
 SNAKE_X = SNAKE_LENGTH + 1
 SNAKE_Y = 3
-TIMEOUT = 100
+TIMEOUT = 100 #this controls the speed of the game
 
 ''' #3 make Snake Oject '''
 class Snake(object):
